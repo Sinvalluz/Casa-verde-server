bac31fb9b2ddf6344a573b6e71112be769b10724
Sinval Luz <contato.sinvalluz@gmail.com>
2024-07-25 19:57:25 -0300

    adding image upload to server

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,25 @@ import cors from '@fastify/cors';
 import offersRoute from './routes/offers.js';
 import souceRouter from './routes/source.js';
 import { config } from 'dotenv';
+import fastifyStatic from '@fastify/static';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 config();
 
 const server = fastify({ logger: true });
 const port = process.env.PORT;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 server.register(cors, {
 	origin: 'http://localhost:5173',
 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
+
+server.register(fastifyStatic, {
+	root: path.join(__dirname, 'public'),
+	prefix: '/public/',
 });
 
 server.register(emailRouter);
