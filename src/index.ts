3a7da864cf7cc2d6e659cbce7aea0cbc0e163ff5
Sinval Luz <contato.sinvalluz@gmail.com>
2024-07-25 09:31:34 -0300

    Adding fastify/cors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-import fastify from "fastify";
-import emailRouter from "./routes/email.js";
+import fastify from 'fastify';
+import emailRouter from './routes/email.js';
+import cors from '@fastify/cors';
 
-const server = fastify();
+const server = fastify({ logger: true });
+
+server.register(cors, {
+	origin: 'http://localhost:5173',
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
 
 server.register(emailRouter);
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
+server.get('/ping', async (request, reply) => {
+	return 'pong\n';
 });
 
 server.listen({ port: 3333 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	console.log(`Server listening at ${address}`);
 });
